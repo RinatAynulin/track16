97ecf5ab64e0d75498cad8456c94bc1bc5ce1185
Rinat Aynulin <rinataynulin@gmail.com>
2016-11-23 08:05:52 +0300

    add DbService singleton

---
+++ src/main/java/track/messenger/db/services/DbService.java
@@ -0,0 +1,33 @@
+package track.messenger.db.services;
+
+/**
+ * Created by Aynulin on 23.11.2016.
+ */
+public class DbService {
+    private static volatile DbService instance;
+
+    private UserService userService;
+    private ChatService chatService;
+    private MessageService messageService;
+
+    private DbService(UserService userService, ChatService chatService, MessageService messageService) {
+        this.userService = userService;
+        this.chatService = chatService;
+        this.messageService = messageService;
+    }
+
+    public static DbService getInstance() {
+        DbService localInstance = instance;
+        if (localInstance == null) {
+            synchronized (DbService.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new DbService(new UserService(),
+                            new ChatService(), new MessageService());
+                }
+            }
+        }
+        return localInstance;
+    }
+
+}
